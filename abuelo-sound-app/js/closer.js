930ae5c01c22ede71000fcc9260ed807935b1955
jillian-milton <jillianmilton@yahoo.com>
2023-08-06 13:28:47 -0400

    sound works now

--- abuelo-sound-app/js/closer.js
+++ abuelo-sound-app/js/closer.js
@@ -1,13 +1,14 @@
+var sample = document.getElementById("audio");
+
 function playSound(){
-    var sample = document.getElementById("audio");
     sample.play();
 }
-
-window.addEventListener('click', playSound());
 
 
 let home = document.querySelector('.home');
 
-home.addEventListener('click', function(){
-    window.location = 'index.html';
-}); 
+home.addEventListener('click', playSound());
+
+sample.onended = function() {
+   window.location = "index.html"; 
+  };
