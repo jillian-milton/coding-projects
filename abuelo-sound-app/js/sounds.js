6880cb0cd34548b1ca1ac8f02f4200317e2f71df
jillian-milton <jillianmilton@yahoo.com>
2023-08-10 22:32:40 -0400

    new feature

--- abuelo-sound-app/js/sounds.js
+++ abuelo-sound-app/js/sounds.js
@@ -4,7 +4,135 @@ back.addEventListener('click', function(){
     window.location = "home.html"; 
 })
 
-function playSound(){
-    var sample = document.getElementById("audio");
-    sample.play();
-}
+var sound1 = new Audio();
+sound1.src = "../audio/Abuelo Jingle.mp3";
+
+sound1.onended = function() {
+    window.location = "sound-1.html"; 
+   };
+
+var sound2 = new Audio();
+sound2.src = "../audio/Very Stupid Idea.mp3";
+
+sound2.onended = function() {
+    window.location = "sound-2.html"; 
+   };
+
+var sound3 = new Audio();
+   sound3.src = "../audio/Why do I need the WiFi.mp3";
+   
+   sound3.onended = function() {
+       window.location = "sound-3.html"; 
+      };
+
+var sound4 = new Audio();
+      sound4.src = "../audio/BINGO!.mp3";
+      
+      sound4.onended = function() {
+          window.location = "sound-4.html"; 
+         };
+
+var sound5 = new Audio();
+         sound5.src = "../audio/Ok, you the boss.mp3";
+         
+         sound5.onended = function() {
+             window.location = "sound-5.html"; 
+            };
+
+var sound6 = new Audio();
+        sound6.src = "../audio/Bird Whistle.mp3";
+            
+        sound6.onended = function() {
+            window.location = "sound-6.html"; 
+        };
+
+var sound7 = new Audio();
+        sound7.src = "../audio/Abuelo Song.mp3";
+            
+        sound7.onended = function() {
+            window.location = "sound-7.html"; 
+        };
+
+var sound8 = new Audio();
+        sound8.src = "../audio/I REPEAT!.mp3";
+            
+        sound8.onended = function() {
+            window.location = "sound-8.html"; 
+        };
+
+var sound9 = new Audio();
+        sound9.src = "../audio/BO Menendez.mp3";
+            
+        sound9.onended = function() {
+            window.location = "sound-9.html"; 
+        };
+
+    var sound10 = new Audio();
+        sound10.src = "../audio/Hacking Noise.mp3";
+            
+        sound10.onended = function() {
+            window.location = "sound-10.html"; 
+        };
+
+        var sound11 = new Audio();
+        sound11.src = "../audio/Ya Ya Ya.mp3" 
+        
+        sound11.onended = function() {
+            window.location = "sound-11.html"; 
+           };
+        
+        var sound12 = new Audio();
+        sound12.src = "../audio/Abuelo 13.mp3";
+        
+        sound12.onended = function() {
+            window.location = "sound-12.html"; 
+           };
+        
+        var sound13 = new Audio();
+           sound13.src = "../audio/Calmate.mp3" ;
+           
+           sound13.onended = function() {
+               window.location = "sound-13.html"; 
+              };
+        
+        var sound14 = new Audio();
+              sound14.src = "../audio/Chila.m4a" ;
+              
+              sound14.onended = function() {
+                  window.location = "sound-14.html"; 
+                 };
+        
+        var sound15 = new Audio();
+                 sound15.src = "../audio/Arriba.m4a" ;
+                 
+                 sound15.onended = function() {
+                     window.location = "sound-15.html"; 
+                    };
+        
+        var sound16 = new Audio();
+                    sound16.src = "../audio/De Nada.m4a" ;
+                    
+                    sound16.onended = function() {
+                        window.location = "sound-16.html"; 
+                       };
+        
+        var sound17  = new Audio();
+                       sound17.src = "../audio/Stupid Question.m4a" ;
+                       
+                       sound17.onended = function() {
+                           window.location = "sound-17.html"; 
+                          };
+        
+        var sound18 = new Audio();
+                 sound18.src = "../audio/Yes sir.m4a" ;
+                 
+                 sound18.onended = function() {
+                     window.location = "sound-18.html"; 
+                    };
+        
+              
+           
+        
+
+
+
